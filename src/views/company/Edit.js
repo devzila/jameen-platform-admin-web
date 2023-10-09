3c163ab5816c9930ba113742d00ffb3f18b39c3f
nilay <nilayanand@gmail.com>
2023-10-09 18:41:26 +0530

    use-form implementation

--- src/views/company/Edit.js
+++ src/views/company/Edit.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react"
+import { useParams } from 'react-router-dom'
+import { useForm } from "react-hook-form"
+import useFetch from 'use-http'
 
 // react-bootstrap components
 import {
@@ -14,10 +17,36 @@ import {
 } from "react-bootstrap";
 
 function Edit() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const { id } = useParams()
+  const { get, put, response, loading, error } = useFetch()
+  const [companyData, setCompanyData] = useState({})
 
   useEffect(()=>{
+    loadComapny()
+  }, [id])
 
-  }, [])
+  async function loadComapny() {
+    const api = await get(`/v1/platform_admin/companies/${id}`)
+    if (response.ok) {
+      setCompanyData(api.data.company)
+    }
+  }
+
+  async function onSubmit(data) { 
+    console.log(data)
+    const api = await put(`/v1/platform_admin/companies/${id}`, {company: data})
+    if (response.ok) {
+      
+    }
+  }
+
   return (
     <>
       <Container fluid>
@@ -28,15 +57,16 @@ function Edit() {
                 <Card.Title as="h4">Edit Company</Card.Title>
               </Card.Header>
               <Card.Body>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}> 
                   <Row>
                     <Col className="pr-1" md="12">
                       <Form.Group>
                         <label>Name</label>
                         <Form.Control
-                          defaultValue=""
+                          defaultValue={companyData.name}
                           placeholder="Company Name"
                           type="text"
+                          {...register("name")}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -46,9 +76,10 @@ function Edit() {
                     <Form.Group>
                         <label>Identifier (No space, No special letter)</label>
                         <Form.Control
-                          defaultValue=""
+                          defaultValue={companyData.slug}
                           placeholder="Identifier"
                           type="text"
+                          {...register("slug")}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -58,9 +89,10 @@ function Edit() {
                       <Form.Group>
                         <label>Scheme</label>
                         <Form.Control
-                          defaultValue=""
+                          defaultValue={companyData.subscription?.name}
                           placeholder="Subscription Scheme"
                           type="text"
+                          {...register("subscription_id")}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -70,7 +102,7 @@ function Edit() {
                     type="submit"
                     variant="info"
                   >
-                    Update Profile
+                    Update
                   </Button>
                   <div className="clearfix"></div>
                 </Form>
